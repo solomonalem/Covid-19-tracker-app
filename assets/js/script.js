1e223eb0daf83c9b383ff145aafe76df71d1ec7c
Solomon <solomonmeresa8@gmail.com>
2020-09-04 20:04:52 -0700

    adds chart,symptoms and arrow icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log("heuen");
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Year", "Cases"],
+    ["", 300],
+    ["", 460],
+    ["", 500],
+    ["", 490],
+    ["", 750],
+    ["", 700],
+    ["", 880],
+    ["", 1120],
+    ["", 1040],
+    ["", 940],
+    ["", 690],
+    ["", 740],
+    ["", 440],
+    ["", 340],
+    ["", 240],
+  ]);
+
+  var options = {
+    title: "",
+    curveType: "function",
+    legend: { position: "bottom" },
+  };
+
+  var chart = new google.visualization.LineChart(
+    document.getElementById("curve_chart")
+  );
+
+  chart.draw(data, options);
+}
